5fa76af89f979b5fafcf028f2c15e9799cff547d
Chrimau <adamazijuemma@gmail.com>
2025-09-03 17:42:17 +0200

    updated the provider file

--- providers.tf
+++ providers.tf
@@ -1,9 +1,13 @@
 terraform {
-    required_version = ">= 1.6"
-    required_providers {
-        azurerm = {
-            source = "hashicorp/azurerm", version = ">=3.100.0"
-        }
+  required_version = ">= 1.6"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 3.100.0"
     }
+  }
 }
-provider "azurerm" { features {} }
+
+provider "azurerm" {
+  features {}
+}
